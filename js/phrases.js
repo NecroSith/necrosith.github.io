c4557e6663c37f1cbe8c9c721d4e3bb20f4653e4
NecroSith <NecroSith@yandex.ru>
2018-01-29 12:19:31 +0300

    Update phrases.js

--- js/phrases.js
+++ js/phrases.js
@@ -1,7 +1,7 @@
 
 (function() {
 	
-	var phrases = ['How does it feel to be locked inside another dream that never had a chance of being realized? (c)', 'You cannot kill what you did not create', 'I took a life worth living and made it worth a mockery', 'Speed defines success'];
+	var phrases = ['How does it feel to be locked inside another dream that never had a chance of being realized? (c) Corey Taylor', 'You cannot kill what you did not create', 'I took a life worth living and made it worth a mockery', 'Speed defines success', 'Arguing that you don\'t care about the right to privacy because you have nothing to hide is no different than saying you don\'t care about free speech because you have nothing to say (c) Edward Snowden'];
 	
 	this.init = function() {
 		getRandomPhrase();
